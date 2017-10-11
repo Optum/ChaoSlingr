f91956ce3fb41acf5c1a5524eadeb8c434f30709
Michael Zhou <michael.zhou@optum.com>
2017-10-11 19:58:04 -0400

    remove old terraform tests

--- test/PortChange_Generatr/variables.tf
+++
@@ -1,7 +0,0 @@
-# Common inputs used across modules
-
-variable "default_tags" { default = {} }
-
-variable "default_tags_true_tag" { default = {} }
-
-variable "default_tags_false_tag" { default = {} }
--- test/PortChange_Slingr/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "security_group_id_truetag" {
-  value = "${aws_security_group.portchange_slingr_test_truetag.id}"
-}
